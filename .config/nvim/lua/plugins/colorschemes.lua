88e34d8c7d728e354e4de046b17ee659a75d2298
Kirill Sadovoy <kirill.sadovoy@Kirills-MacBook-Pro.local>
2025-01-29 00:53:15 +0000

    update telescope config, format rest of the files

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
+  'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
+    -- load the colorscheme here
+    vim.cmd [[colorscheme tokyonight]]
+  end,
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,21 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function()
+    require('telescope').setup {
+      pickers = {
+        find_files = {
+          theme = 'ivy',
+        },
+      },
+    }
+
+    local builtin = require 'telescope.builtin'
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+  end,
 }
