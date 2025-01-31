652cfd1a17c80b21260cad906e69e30ccd841a1a
Kirill Sadovoy <kirill.sadovoy@Kirills-MacBook-Pro.local>
2025-01-31 23:47:06 +0000

    use letters instead icons in lualine for diagnostics

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,7 @@ return {
         component_separators = '',
         section_separators = { left = '', right = '' },
         disabled_filetypes = { 'neo-tree' },
+        symbols = { error = 'E|', warn = 'W|', info = 'I|', hint = 'H|' },
       },
     }
   end,
