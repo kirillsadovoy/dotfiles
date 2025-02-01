1a3c15b17ad1ef5e9eb6f60efe3b276a502e5ef6
Kirill Sadovoy <kirill.sadovoy@Kirills-MacBook-Pro.local>
2025-02-01 23:17:08 +0000

    use telescope for code-actions, udpate confirm mappings for insert mode, add find grep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,16 +4,22 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-ui-select.nvim',
   },
   config = function()
     require('telescope').setup {
+      defaults = require('telescope.themes').get_ivy {
+        mappings = {
+          i = {
+            ['<C-y>'] = 'select_default',
+          },
+        },
+      },
       extensions = {
         wrap_results = true,
         fzf = {},
-      },
-      pickers = {
-        find_files = {
-          theme = 'ivy',
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown(),
         },
       },
     }
@@ -21,8 +27,10 @@ return {
     local builtin = require 'telescope.builtin'
 
     require('telescope').load_extension 'fzf'
+    require('telescope').load_extension 'ui-select'
 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   end,
 }
